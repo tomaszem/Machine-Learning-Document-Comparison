82353e958f0ca911e03fffedd08be722b257f86c
Tomas Zeman <zemant24@gmail.com>
2024-01-12 11:34:49 +0100

    Add data storage class for RavenDB

---
+++ data_storage.py
@@ -0,0 +1,19 @@
+from ravendb import DocumentStore
+from document_distance import DocumentDistance, DocumentComparison
+from distance_calculator import calculate_euclidean_distances
+
+class DataStorage:
+    def __init__(self, store):
+        self.store = store
+
+    def store_document_distances(self, dist_matrix, filenames):
+        with self.store.open_session() as session:
+            comparisons = calculate_euclidean_distances(dist_matrix, filenames)
+
+            for i, filename in enumerate(filenames):
+                document_comparisons = [DocumentComparison(target_document=other_file, distance=dist)
+                                        for dist, other_file in comparisons[i]]
+                document_distances = DocumentDistance(document_id=filename, comparisons=document_comparisons)
+                session.store(document_distances)
+
+            session.save_changes()
