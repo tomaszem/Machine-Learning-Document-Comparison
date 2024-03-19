edb81db8a29631f97d659e43f110d0001daa9e55
Tomas Zeman <zemant24@gmail.com>
2024-03-19 18:34:28 +0100

    Add UMAP as an algorithm option into clustering process

--- app/clustering_operations.py
+++ app/clustering_operations.py
@@ -2,6 +2,7 @@ from sklearn.decomposition import PCA
 from app.extract_text import load_texts_from_pdfs_batched
 from app.tf_idf_algorithm import custom_vectorization
 from sklearn.cluster import DBSCAN
+from umap import UMAP
 
 
 def perform_clustering():
@@ -9,13 +10,20 @@ def perform_clustering():
     BATCH_SIZE = 5
     texts, filenames = load_texts_from_pdfs_batched(BATCH_SIZE)
 
-    custom_weights = {"java": 12, "javascript": 5, "python": 9, "algebra": 3, "numerical": 3, "sql": 10}
+    # custom_weights = {"algebra": 3, "numerical": 3, "sql": 10}
+    custom_weights = {}
     custom_vectors = custom_vectorization(texts, custom_weights)
+
+    # UMAP for dimensionality reduction
+    #umap = UMAP(n_components=2)
+    #reduced_vectors = umap.fit_transform(custom_vectors)
+
     pca = PCA(n_components=2)
     reduced_vectors = pca.fit_transform(custom_vectors)
 
     # DBSCAN clustering
-    dbscan = DBSCAN(eps=0.5, min_samples=2)
+    # PCA 0.05; 0.08
+    dbscan = DBSCAN(eps=0.08, min_samples=2)
     clusters = dbscan.fit_predict(reduced_vectors)
 
     return filenames, reduced_vectors, clusters
