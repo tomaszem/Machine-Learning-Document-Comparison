ec375a1d6baea1c30623069eeba38032015a0dd3
Tomas Zeman <zemant24@gmail.com>
2024-04-16 18:11:06 +0200

    Integration of set_vector_weights

--- app/set_vector_weights.py
+++ app/set_vector_weights.py
@@ -2,29 +2,20 @@ import re
 from nltk.corpus import stopwords
 from app.pdf_info_extraction import get_pdf_details
 
-stop_words = set(stopwords.words('english'))
 
+def vector_weights(references_list, word_weight=1.2):
+    references_text = ''.join(references_list)
 
-def vector_weights():
-    custom_weights = {}
-
-    all_pdf_details = get_pdf_details()
-    custom_references = [detail['references'] for detail in all_pdf_details]
-
-    references_text = ''.join([ref for sublist in custom_references if sublist for ref in sublist if ref])
-
+    # Define stop words
     stop_words = set(word.lower() for word in stopwords.words('english'))
+
+    # Collect unique words
     all_unique_words = set()
-
     word_regex = r'\b[A-Za-z]+\b'
-
     words = re.findall(word_regex, references_text)
     all_unique_words.update([word.lower() for word in words if word.lower() not in stop_words and len(word) > 1])
 
-    custom_weights = {word: 1.3 for word in all_unique_words}
+    # Assign custom weight to each word
+    custom_weights = {word: word_weight for word in all_unique_words}
 
-    # TODO return custom_weights
-    # custom_vectors = custom_vectorization(texts, custom_weights)
-
-
-vector_weights()
+    return custom_weights
